20d46acaeaea09bbe07bf4db4aa3fb64ecbb4425
MariamAntsupova <mariam.antsupova.1@btu.edu.ge>
2021-07-12 02:14:33 +0400

    'singlee'

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -3,77 +3,195 @@ import useStyles from '../Layouts/CardsLayout/CardsLayoutStyles';
 import image from '../Assets/Images/12.jpg';
 import { SinglePage } from '../Routes/Routes';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
-
-const cards = [
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-    {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-        {
-        image:{image},
-        title: "Black denim jacket",
-        price: "$99.99",
-    },
-];
 
 
 function CardComponent() {
   const classes = useStyles();
 
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      title: "Black denim jacket",
+      price: "$99.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Blue",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 2,
+      title: "Grey sweater",
+      price: "$21.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Grey",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 3,
+      title: "Blue denim shirt",
+      price: "$17.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Blue",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 4,
+      title: "Blue denim shirt",
+      price: "$17.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Blue",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 5,
+      title: "Red hoodie",
+      price: "$35.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Red",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 6,
+      title: "Black denim jacket",
+      price: "$99.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Blue",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 7,
+      title: "Grey sweater",
+      price: "$21.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Grey",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 8,
+      title: "Blue denim shirt",
+      price: "$17.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Blue",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+    {
+      id: 9,
+      title: "Blue denim shirt",
+      price: "$17.99",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
+      category: "Shirt",
+      model: "Shirt 5407x",
+      color: "Blue",
+      delivery: "USA, Europe",
+      image: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg",
+      imgGroup: [
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
+        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+      ],
+    },
+  ]);
+  
   return (
     <>
-              {cards.map((card)=>(
-                <Grid item key={card} xs={12} sm={6} md={4} >
+              {cards.map((el, index) => (
+                <Grid item key={index} xs={12} sm={6} md={4} >
                    <Link to={SinglePage} style={{textDecoration: 'none'}}> 
                     <Card className={classes.card} >
                         <CardMedia
                           className={classes.cardMedia}
-                          image={image}
+                          image={el.image}
                           title='Image Title'
                         />
                         <CardContent className={classes.cardContent}>
                           <Typography gutterBottom variant='h5'>
-                            Black denim jacket
+                            {el.title}
                           </Typography>
                           <Typography >
-                            $99.99
+                            {el.price}
                           </Typography>
                         </CardContent>
                     </Card>
--- src/Layouts/CardsLayout/CardsLayout.js
+++ src/Layouts/CardsLayout/CardsLayout.js
@@ -6,118 +6,6 @@ import React, { useState } from 'react';
 
 function Cards() {
   const classes = useStyles();
-  const [cards, setCards] = useState([
-    {
-      title: "Black denim jacket1",
-      price: "99,99",
-      disCountPrice: "21,99",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
-      category: "Shirt",
-      review: 3,
-      model: "Shirt 5407x",
-      color: "Blue",
-      delivery: "USA, Europe",
-      size: ["small", "medium", "large"],
-      img: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
-      imgGroup: [
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
-      ],
-      id: 1
-    },
-    {
-      title: "Black denim jacket2",
-      price: "99,99",
-      disCountPrice: false,
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
-      category: "Shirt",
-      reviewCount: 7,
-      review: 2,
-      model: "Shirt 5407x",
-      color: "Blue",
-      delivery: "USA, Europe",
-      size: ["small", "medium", "large"],
-      img: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
-      imgGroup: [
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
-      ],
-      id: 2
-    },
-    {
-      title: "Black denim jacket3",
-      price: "99",
-      disCountPrice: false,
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
-      category: "Shirt",
-      reviewCount: 4,
-      review: 5,
-      model: "Shirt 5407x",
-      color: "Blue",
-      delivery: "USA, Europe",
-      size: ["small", "medium", "large"],
-      img: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
-      imgGroup: [
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
-      ],
-      id: 3
-    },
-    {
-      title: "Black denim jacket4",
-      price: "59,21",
-      disCountPrice: "31,99",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
-      category: "Shirt",
-      reviewCount: 10,
-      review: 4,
-      model: "Shirt 5407x",
-      color: "Blue",
-      delivery: "USA, Europe",
-      size: ["small", "medium", "large"],
-      img: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
-      imgGroup: [
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
-      ],
-      id: 4
-    },
-    {
-      title: "Black denim jacket5",
-      price: "99,18",
-      disCountPrice: false,
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.",
-      category: "Shirt",
-      reviewCount: 2,
-      review: 1,
-      model: "Shirt 5407x",
-      color: "Blue",
-      delivery: "USA, Europe",
-      size: ["small", "medium", "large"],
-      img: "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15.jpg",
-      imgGroup: [
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg",
-        "https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
-      ],
-      id: 5
-    }
-  ]);
-
   return (
     <>
         <CssBaseline />
@@ -139,7 +27,7 @@ function Cards() {
                 </Box>
             </Grid >
             <Grid container spacing={4}>
-              <CardComponent cards={cards} />
+              <CardComponent/>
             </Grid>
             <Grid item xs={12} className={classes.grid}>
                 <Grid item xs={3}>
