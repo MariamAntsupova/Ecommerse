d7fd032de41b9656111e1eef19280a98e53f846f
MariamAntsupova <mariam.antsupova.1@btu.edu.ge>
2021-08-14 17:57:25 +0400

    'login'

--- src/store/user/userAction.js
+++ src/store/user/userAction.js
@@ -1,23 +1,25 @@
 import {setToken, setUser} from "./userActionCreator";
 
-export const loginUser = (email, password) =>  (dispatch, getState) => {
+export const loginUser = (body) =>  (dispatch, getState) => {
+    console.log(body)
     fetch('http://159.65.126.180/api/auth/login', {
         method: 'POST',
         headers: {
-            "Content-Type": "application/json",
-            accept: "application/json",
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
-        body: JSON.stringify({email, password}),
+        body: JSON.stringify(body),
     }).then((res) => {
-        console.log(res.json());
         if (res.ok) {
                 console.log(res)
-                dispatch(setUser(res.user))
-                dispatch(setToken(res.token.access_token))
                 return res.json()
             } else {
                 console.log('problem');
             }
+        }).then(data => {
+            console.log(data)
+            dispatch(setUser(data.user))
+            dispatch(setToken(data.token.access_token))
         })
 
 }
